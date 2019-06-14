d8980c7eceebfb7e9a0b261b20498f13ddfcdd36
kenchangh <guanhao3797@gmail.com>
2019-06-15 00:47:49 +0800

    new deployment

--- analytics/src/firebase.js
+++ analytics/src/firebase.js
@@ -2,9 +2,9 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCT-9SIQICMO14zbYNsQ-pm5Tkj3WOP7Fg",
+  apiKey: "AIzaSyCiuEW53FG7l34-usnKz4aNW7C850OVj9I",
   // authDomain: "### FIREBASE AUTH DOMAIN ###",
-  projectId: "anconia-4008c"
+  projectId: "anconia-project"
 });
 
 export const db = firebase.firestore();
